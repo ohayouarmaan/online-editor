071aaf4773968d66e1fe2edd36bc9080b94a8a52
Armaan Gupta <ohayouarmaan@gmail.com>
2023-02-24 21:51:16 +0530

    first commit

---
+++ Editor.py
@@ -0,0 +1,47 @@
+# Import statements
+import ffmpeg
+import os
+import toml
+
+# Helper classes
+class Media:
+    def __init__(self, path: str):
+        self.path = path
+        if(os.path.isfile(self.path)):
+            self.inp = ffmpeg.input(self.path)
+        else:
+            self.inp = None
+            raise FileNotFoundError()
+
+    def toJSON(self) -> object:
+        return {
+            "path": self.path,
+        }
+
+class Timeline:
+    def __init__(self, medias: list[Media]):
+        self.medias = medias
+        
+    def add(self, media: Media) -> bool:
+        if media:
+            self.medias.append(media)
+            return True
+        else:
+            return False
+
+class Window:
+    def __init__(self):
+        self.elems = []
+    
+    def add(self, elem):
+        self.elems.append(elem)
+
+class Editor:
+    def __init__(self):
+        self.config = toml.load("./CONFIGURATION.toml")['Editor_Configurations']
+        self.__version__ = self.config['version']
+        self.default_window = Window()
+
+if __name__ == "__main__":
+    e = Editor()
+    print(e.config)
